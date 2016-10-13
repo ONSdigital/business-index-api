a42e2b91622fc9e18742b216fe50ef2a22fa4e58
Mathias Bogaert <mbogaert@nephilaadvisors.co.uk>
2016-10-13 14:57:12 +0100

    Cleanup.

--- app/services/InsertDemoData.scala
+++ app/services/InsertDemoData.scala
@@ -9,13 +9,19 @@ import play.api.inject.ApplicationLifecycle
 import com.sksamuel.elastic4s.ElasticDsl._
 import com.sksamuel.elastic4s.analyzers._
 import com.sksamuel.elastic4s.mappings.FieldType._
+import play.api.{Environment, Mode}
 
 import scala.io.Source
 
+/**
+  * Class that imports sample.csv.
+  *
+  * CSV file header: "ID","BusinessName","UPRN","IndustryCode","LegalStatus","TradingStatus","Turnover","EmploymentBands"
+  */
 @Singleton
-class InsertDemoData @Inject()(client: ElasticClient, appLifecycle: ApplicationLifecycle) {
+class InsertDemoData @Inject()(environment: Environment, client: ElasticClient, appLifecycle: ApplicationLifecycle) {
   client.execute {
-    // "ID","BusinessName","UPRN","IndustryCode","LegalStatus","TradingStatus","Turnover","EmploymentBands"
+    // define the ElasticSearch index
     create.index("bi").mappings(
       mapping("business").fields(
         field("BusinessName", StringType) boost 4 analyzer "BusinessNameAnalyzer",
@@ -25,38 +31,44 @@ class InsertDemoData @Inject()(client: ElasticClient, appLifecycle: ApplicationL
         field("TradingStatus", IntegerType) index "not_analyzed" includeInAll false,
         field("Turnover", StringType) index "not_analyzed" includeInAll false,
         field("EmploymentBands", StringType) index "not_analyzed" includeInAll false
-        )
+      )
     ).analysis(CustomAnalyzerDefinition("BusinessNameAnalyzer",
       StandardTokenizer("BusinessNameStandardTokenizer", 20),
       LowercaseTokenFilter,
       NGramTokenFilter("BusinessNameNgramFilter", minGram = 1, maxGram = 3)))
   }
 
-  readResourceFile("/demo/sample.csv").filter(!_.contains("BusinessName")).foreach { line =>
-    val values = line.replace("\"", "").split(",")
+  // if in dev mode, import the file sample.csv
+  environment.mode match {
+    case Mode.Dev =>
+      readResourceFile("/demo/sample.csv").filter(!_.contains("BusinessName")).foreach { line =>
+        val values = line.replace("\"", "").split(",")
 
-    client.execute {
-      index into "bi" / "business" id values(0) fields(
-        "BusinessName" -> values(1),
-        "UPRN" -> values(2).toLong,
-        "IndustryCode" -> values(3).toLong,
-        "LegalStatus" -> values(4).toInt,
-        "TradingStatus" -> values(5).toInt,
-        "Turnover" -> values(6),
-        "EmploymentBands" -> values(7))
-    }
+        client.execute {
+          index into "bi" / "business" id values(0) fields(
+            "BusinessName" -> values(1),
+            "UPRN" -> values(2).toLong,
+            "IndustryCode" -> values(3).toLong,
+            "LegalStatus" -> values(4).toInt,
+            "TradingStatus" -> values(5).toInt,
+            "Turnover" -> values(6),
+            "EmploymentBands" -> values(7))
+        }
+      }
+
+      println("Inserted DEMO data.")
+
+      appLifecycle.addStopHook { () =>
+        client.execute {
+          delete index "bi"
+        }
+      }
+    case Mode.Test =>
+    case Mode.Prod =>
   }
 
   def readResourceFile(p: String): List[String] =
     Option(getClass.getResourceAsStream(p)).map(Source.fromInputStream)
       .map(_.getLines.toList)
       .getOrElse(throw new FileNotFoundException(p))
-
-  println("Inserted DEMO data.")
-
-  appLifecycle.addStopHook { () =>
-    client.execute {
-      delete index "bi"
-    }
-  }
 }
