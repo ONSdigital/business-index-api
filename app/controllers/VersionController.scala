c446d51fa04d07a1fe1bdbbfe9bd91bd82f9988e
Mathias Bogaert <mbogaert@nephilaadvisors.co.uk>
2016-10-18 09:52:39 +0100

    Sort buildinfo map by keys.

--- app/controllers/VersionController.scala
+++ app/controllers/VersionController.scala
@@ -5,6 +5,7 @@ import javax.inject.{Inject, Singleton}
 import com.typesafe.scalalogging.StrictLogging
 import play.api.mvc.{Action, Controller}
 
+import scala.collection.immutable.ListMap
 import scala.concurrent.ExecutionContext
 
 @Singleton
@@ -12,6 +13,8 @@ class VersionController @Inject()(implicit exec: ExecutionContext)
   extends Controller with StrictLogging {
 
   def version = Action {
-    Ok(BuildInfo.toJson).as("application/json")
+    Ok(ListMap(BuildInfo.toMap.toSeq.sortBy(_._1):_*)
+      .map(i => "\"" + i._1 + "\":\"" + i._2 + "\"").mkString("{", ", ", "}")
+    ).as("application/json")
   }
 }
