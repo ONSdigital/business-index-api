36c0e1293d34b764faa89bb50ac401fed9e4ce7f
Mathias Bogaert <mbogaert@nephilaadvisors.co.uk>
2016-10-31 13:09:28 +0000

    Cleanup the searching methods. Add X-Total-Count header.

--- app/controllers/v1/SearchController.scala
+++ app/controllers/v1/SearchController.scala
@@ -45,45 +45,24 @@ class SearchController @Inject()(environment: Environment, elasticSearch: Elasti
     }
   }
 
-  def suggestBusiness = Action.async { implicit request =>
+  def searchBusiness(suggest: Boolean) = Action.async { implicit request =>
     val start = Try(request.getQueryString("start").getOrElse("0").toInt).getOrElse(0)
     val limit = Try(request.getQueryString("limit").getOrElse("100").toInt).getOrElse(100)
 
     request.getQueryString("q").orElse(request.getQueryString("query")) match {
       case Some(query) if query.length > 0 =>
+        val definition = if (suggest) matchQuery("BusinessName", query) else QueryStringQueryDefinition(query)
         elasticSearch.execute {
           search.in(s"bi-${environment.mode.toString.toLowerCase}" / "business")
-            .query(matchQuery("BusinessName", query))
+            .query(definition)
             .start(start)
             .limit(limit)
         }.map { elasticSearchResponse =>
           val businesses = elasticSearchResponse.as[Business]
-          if (businesses.length > 0) Ok(Json.toJson(businesses))
-          else NoContent
-        }.recover {
-          case e: NoNodeAvailableException => ServiceUnavailable(Json.obj("status" -> 503, "code" -> "es_down", "message_en" -> e.getMessage))
-          case NonFatal(e) => InternalServerError(Json.obj("status" -> 500, "code" -> "internal_error", "message_en" -> e.getMessage))
-        }
-      case _ =>
-        Future.successful(BadRequest(Json.obj("status" -> "400", "code" -> "missing_query", "message_en" -> "No query specified.")))
-    }
-  }
-
-  def searchBusiness = Action.async { implicit request =>
-    val start = Try(request.getQueryString("start").getOrElse("0").toInt).getOrElse(0)
-    val limit = Try(request.getQueryString("limit").getOrElse("100").toInt).getOrElse(100)
-
-    request.getQueryString("q").orElse(request.getQueryString("query")) match {
-      case Some(query) if query.length > 0 =>
-        elasticSearch.execute {
-          search.in(s"bi-${environment.mode.toString.toLowerCase}" / "business")
-            .query(query)
-            .start(start)
-            .limit(limit)
-        }.map { elasticSearchResponse =>
-          val businesses = elasticSearchResponse.as[Business]
-          if (businesses.length > 0) Ok(Json.toJson(businesses))
-          else NoContent
+          if (businesses.length > 0)
+            Ok(Json.toJson(businesses)).withHeaders("X-Total-Count" -> elasticSearchResponse.totalHits.toString)
+          else
+            Ok("{}").as("application/json")
         }.recover {
           case e: NoNodeAvailableException => ServiceUnavailable(Json.obj("status" -> 503, "code" -> "es_down", "message_en" -> e.getMessage))
           case NonFatal(e) => InternalServerError(Json.obj("status" -> 500, "code" -> "internal_error", "message_en" -> e.getMessage))
