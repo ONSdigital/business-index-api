cf80aa9bc32c730b4cf814679b27838a4d05b436
Mathias Bogaert <mathias.bogaert@gmail.com>
2016-11-09 10:53:10 +0000

    Add ES health and health endpoint.

--- app/Module.scala
+++ app/Module.scala
@@ -2,6 +2,7 @@ import javax.inject.Inject
 
 import ch.qos.logback.classic.LoggerContext
 import com.codahale.metrics.JmxReporter
+import com.codahale.metrics.health.HealthCheck
 import com.google.inject.AbstractModule
 import com.sksamuel.elastic4s.{ElasticClient, ElasticsearchClientUri}
 import com.typesafe.scalalogging.StrictLogging
@@ -11,6 +12,8 @@ import org.slf4j.LoggerFactory
 import play.api.inject.ApplicationLifecycle
 import play.api.{Configuration, Environment, Mode}
 import services.InsertDemoData
+import com.sksamuel.elastic4s._
+import org.elasticsearch.cluster.health.ClusterHealthStatus._
 
 import scala.concurrent.Future
 
@@ -23,7 +26,7 @@ class AvoidLogbackMemoryLeak @Inject()(lifecycle: ApplicationLifecycle) extends 
 }
 
 class Module(environment: Environment,
-             configuration: Configuration) extends AbstractModule with DefaultInstrumented {
+             configuration: Configuration) extends AbstractModule with DefaultInstrumented with ElasticDsl {
 
   override def configure() = {
     val elasticSearchClient = environment.mode match {
@@ -53,6 +56,19 @@ class Module(environment: Environment,
     }
 
     bind(classOf[ElasticClient]).toInstance(elasticSearchClient)
+
+    // register Elasticsearch cluster health check
+    healthCheck("es-cluster-alive") {
+      elasticSearchClient.execute {
+        get cluster health
+      }.await match {
+        case response if response.getStatus == GREEN =>
+          HealthCheck.Result.healthy(response.toString)
+        case response =>
+          HealthCheck.Result.unhealthy(response.toString)
+      }
+    }
+
     bind(classOf[InsertDemoData]).asEagerSingleton()
     bind(classOf[AvoidLogbackMemoryLeak]).asEagerSingleton()
 
