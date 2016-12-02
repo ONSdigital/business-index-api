177ddc0cb94cf8cbca08a9df0da57b7c165883dd
Flavian Alexandru <flavian@outworkers.com>
2016-12-02 12:12:42 +0000

    Adding an address

--- app/models/domain/records/PAYERecord.scala
+++ app/models/domain/records/PAYERecord.scala
@@ -1,36 +1,38 @@
 package models.domain.records
 
+import com.outworkers.phantom.dsl.DateTime
 
-case class EmployerAddress(
-  employer_address_line_1: String,
-  employer_address_line_2: String,
-  employer_address_line_3: String,
-  employer_address_line_4: String,
-  employer_address_line_5: String,
+case class Address(
+  line_1: String,
+  line_2: String,
+  line_3: String,
+  line_4: String,
+  line_5: String,
   postcode: String
-)
-
-case class CommunicationAddress(
-  communication_address_line_1: String,
-  communication_address_line_2: String,
-  communication_address_line_3: String,
-  communication_address_line_4: String,
-  communication_address_line_5: String,
-  communication_postcode: String
 )
 
 case class PAYERecord(
   emp_stats_rec_id: String,
   district_number: Int,
   employer_reference: Int,
-  employer_name_line_1: Int,
   accounts_office_reference: Int,
-  address: EmployerAddress,
+  address: Address,
   trading_as_name_1: String,
   trading_as_name_2: String,
   communication_name: String,
-  communication_address: CommunicationAddress,
-  legal_status: String
+  communication_address: Address,
+  legal_status: String,
+  scheme_type: String,
+  date_commenced: DateTime,
+  transfer_in_identifier: String,
+  successor_in_identifier: String,
+  date_ceased: DateTime,
+  transfer_out_identifier: String,
+  merger_out_identifier: String,
+  succession_out_identifier: String,
+  date_of_transfer: DateTime,
+  scheme_cancelled_date: DateTime,
+  scheme_reopened_date:DateTime
 ) {
   def trade_classification_number: String = legal_status
 }
