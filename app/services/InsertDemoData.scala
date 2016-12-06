d3ce5d540339bb41beb84c0469a3b460b7fa5992
Flavian Alexandru <alexflav23@users.noreply.github.com>
2016-12-06 12:43:23 +0000

    Adding the ability to provide search term as part of the path (#7)
    * Adding the ability to provide search term as part of the path
    
    * Re-adding recovery behaviour
    
    * Automatically inserting data in prod environment for now

--- app/services/InsertDemoData.scala
+++ app/services/InsertDemoData.scala
@@ -14,9 +14,9 @@ import org.elasticsearch.indices.IndexAlreadyExistsException
 import org.elasticsearch.transport.RemoteTransportException
 import play.api.{Environment, Mode}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.io.Source
-import scala.util.control.NonFatal
+import scala.concurrent.duration._
 
 /**
   * Class that imports sample.csv.
@@ -24,71 +24,81 @@ import scala.util.control.NonFatal
   * CSV file header: "ID","BusinessName","UPRN","IndustryCode","LegalStatus","TradingStatus","Turnover","EmploymentBands"
   */
 @Singleton
-class InsertDemoData @Inject()(environment: Environment,
-                               elasticsearchClient: ElasticClient,
-                               applicationLifecycle: ApplicationLifecycle)
-                              (implicit exec: ExecutionContext) extends StrictLogging {
+class InsertDemoData @Inject()(
+  environment: Environment,
+  elasticsearchClient: ElasticClient,
+  applicationLifecycle: ApplicationLifecycle
+)(
+  implicit exec: ExecutionContext
+) extends StrictLogging {
 
-  elasticsearchClient.execute {
-    // define the ElasticSearch index
-    create.index(s"bi-${environment.mode.toString.toLowerCase}").mappings(
-      mapping("business").fields(
-        field("BusinessName", StringType) boost 4 analyzer "BusinessNameAnalyzer",
-        field("BusinessName_suggest", CompletionType),
-        field("UPRN", LongType) analyzer KeywordAnalyzer,
-        field("IndustryCode", LongType) analyzer KeywordAnalyzer,
-        field("LegalStatus", StringType) index "not_analyzed" includeInAll false,
-        field("TradingStatus", StringType) index "not_analyzed" includeInAll false,
-        field("Turnover", StringType) index "not_analyzed" includeInAll false,
-        field("EmploymentBands", StringType) index "not_analyzed" includeInAll false
-      )
-    ).analysis(CustomAnalyzerDefinition("BusinessNameAnalyzer",
-      StandardTokenizer,
-      LowercaseTokenFilter,
-      edgeNGramTokenFilter("BusinessNameNGramFilter") minGram 2 maxGram 24))
-  }.recover {
-    case _: IndexAlreadyExistsException => // Ok, ignore
-    case _: RemoteTransportException => // Ok, ignore
-  }.await
+  def initialiseIndex: Future[Unit] = {
+    elasticsearchClient.execute {
+      // define the ElasticSearch index
+      create.index(s"bi-${environment.mode.toString.toLowerCase}").mappings(
+        mapping("business").fields(
+          field("BusinessName", StringType) boost 4 analyzer "BusinessNameAnalyzer",
+          field("BusinessName_suggest", CompletionType),
+          field("UPRN", LongType) analyzer KeywordAnalyzer,
+          field("IndustryCode", LongType) analyzer KeywordAnalyzer,
+          field("LegalStatus", StringType) index "not_analyzed" includeInAll false,
+          field("TradingStatus", StringType) index "not_analyzed" includeInAll false,
+          field("Turnover", StringType) index "not_analyzed" includeInAll false,
+          field("EmploymentBands", StringType) index "not_analyzed" includeInAll false
+        )
+      ).analysis(CustomAnalyzerDefinition("BusinessNameAnalyzer",
+        StandardTokenizer,
+        LowercaseTokenFilter,
+        edgeNGramTokenFilter("BusinessNameNGramFilter") minGram 2 maxGram 24))
+    } map { res =>
+      if (environment.mode != Mode.Prod) {
+        applicationLifecycle.addStopHook { () =>
+          elasticsearchClient.execute {
+            delete index s"bi-${environment.mode.toString.toLowerCase}"
+          }
+        }
+      }
+    } recoverWith {
+      case _: IndexAlreadyExistsException => Future.successful(())
+      case e: RemoteTransportException => Future.failed(e)
+    }
+  }
 
-  if (environment.mode != Mode.Prod) {
-    applicationLifecycle.addStopHook { () =>
-      elasticsearchClient.execute {
-        delete index s"bi-${environment.mode.toString.toLowerCase}"
+  def generateData(): List[Array[String]] = {
+    readCSVFile(s"/${environment.mode.toString.toLowerCase}/sample.csv") map { case (line, lineNum) =>
+      line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1).map(_.replace("\"", ""))
+    }
+  }
+
+  def importData(source: List[Array[String]]): Future[List[IndexResult]] = {
+    Future.sequence {
+      source map { values =>
+        elasticsearchClient.execute {
+          index into s"bi-${environment.mode.toString.toLowerCase}" / "business" id values(0) fields(
+            "BusinessName" -> values(1),
+            "UPRN" -> values(2).toLong,
+            "IndustryCode" -> values(3).toLong,
+            "LegalStatus" -> values(4),
+            "TradingStatus" -> values(5),
+            "Turnover" -> values(6),
+            "EmploymentBands" -> values(7))
+        }
       }
     }
   }
 
-  // if in dev mode, import the file sample.csv
+  def init: Future[List[IndexResult]] = for {
+    index <- initialiseIndex
+    data <- importData(generateData())
+  } yield data
+
+  logger.info("Importing sample data in all modes.")
   environment.mode match {
-    case Mode.Dev | Mode.Test =>
-      var imported = 0
-      readCSVFile(s"/${environment.mode.toString.toLowerCase}/sample.csv").foreach { case (line, lineNum) =>
-        val values = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1).map(_.replace("\"", ""))
-        try {
-          elasticsearchClient.execute {
-            index into s"bi-${environment.mode.toString.toLowerCase}" / "business" id values(0) fields(
-              "BusinessName" -> values(1),
-              "UPRN" -> values(2).toLong,
-              "IndustryCode" -> values(3).toLong,
-              "LegalStatus" -> values(4),
-              "TradingStatus" -> values(5),
-              "Turnover" -> values(6),
-              "EmploymentBands" -> values(7))
-          }.await
-
-          imported += 1
-        }
-        catch {
-          case NonFatal(ex) => println(s"Failed importing line $lineNum: $line - ${ex}")
-        }
-      }
-
-      logger.warn(s"Inserted ${environment.mode.toString.toLowerCase} data ($imported entries).")
-    case Mode.Prod =>
+    case Mode.Dev | Mode.Test => Await.result(init, 5.minutes)
+    case Mode.Prod => Await.result(init, 5.minutes)
   }
 
-  private def readCSVFile(p: String): List[(String, Int)] =
+  private[this] def readCSVFile(p: String): List[(String, Int)] =
     Option(getClass.getResourceAsStream(p)).map(Source.fromInputStream)
       .map(_.getLines.filterNot(_.contains("BusinessName")).zipWithIndex.toList)
       .getOrElse(throw new FileNotFoundException(p))
