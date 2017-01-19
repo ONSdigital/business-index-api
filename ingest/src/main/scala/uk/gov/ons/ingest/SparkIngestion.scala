427cf84470adeba9b9c79e062279fc60b6d9ea29
Flavian Alexandru <flavian@outworkers.com>
2017-01-19 18:17:14 +0000

    Adding a more self documenting setup

--- ingest/src/main/scala/uk/gov/ons/ingest/SparkIngestion.scala
+++ ingest/src/main/scala/uk/gov/ons/ingest/SparkIngestion.scala
@@ -1,7 +1,14 @@
 package uk.gov.ons.ingest
 
+import akka.remote.RemoteTransportException
+import com.sksamuel.elastic4s.ElasticClient
 import org.apache.spark.SparkContext
+import org.elasticsearch.indices.IndexAlreadyExistsException
+import org.slf4j.LoggerFactory
+import uk.gov.ons.ingest.writers.ElasticInitializer
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 case class SparkConfig(
   hdfsUrl: String,
@@ -10,9 +17,31 @@ case class SparkConfig(
 )
 
 class SparkIngestion(
+  val elastic: ElasticClient,
+  val indexes: List[ElasticInitializer],
   val context: SparkContext
 ) {
 
-  def setup: Unit = {
+  private[this] val timeoutSeconds = 20
+  private[this] val logger = LoggerFactory.getLogger(getClass)
+
+  protected[this] def shutdown(error: Throwable): Unit = {
+    context.stop()
+    sys.error(error.getMessage)
+  }
+
+  def setup(): Unit = {
+    Await.result(
+      ElasticInitializer(indexes) recover {
+        case e: IndexAlreadyExistsException => {
+          logger.error("Index already exists, but the error can be silenced", e)
+        }
+        case x: RemoteTransportException => {
+          logger.error("Could not connect to Elastic Cluster", x)
+          shutdown(x)
+        }
+      },
+      timeoutSeconds seconds
+    )
   }
 }
