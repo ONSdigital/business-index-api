ee4544fa42fe06709cc95c13b50a487b749771f0
Volodymyr.Glushak <volodymyr.glushak@valtech.co.uk>
2017-03-10 11:22:55 +0000

    Make codacy happy.

--- api/test/scala/JsonParseTest.scala
+++ api/test/scala/JsonParseTest.scala
@@ -1,6 +1,6 @@
 package scala
 
-import controllers.v1.BusinessIndexObj._
+import controllers.v1.BusinessIndexObj.businessHitFormat
 import org.scalatest.{FlatSpec, Matchers}
 import play.api.libs.json.Json
 import uk.gov.ons.bi.models.BusinessIndexRec
@@ -22,7 +22,7 @@ class JsonParseTest extends FlatSpec with Matchers {
     parseJson(fullJson).map(_.id).getOrElse(0L) shouldBe 85282744
     parseJson(smallerJson).map(_.id).getOrElse(0L) shouldBe 85282745
 
-    Json.toJson(parseJson(smallerJson)).toString() shouldBe smallerJson
+    Json.toJson(parseJson(smallerJson).getOrElse(sys.error("Can't parse"))).toString() shouldBe smallerJson
   }
 
   private[this] def parseJson(x: String) = Json.fromJson[BusinessIndexRec](Json.parse(x))
