28606bee887cb3098b0ffcbd4dc89ff32cf6e9a4
Ameen <ameenhaq@users.noreply.github.com>
2017-03-21 10:57:00 +0000

    Updated HealthController.scala
    Modified code to return live status of API in JSON form

--- api/app/controllers/HealthController.scala
+++ api/app/controllers/HealthController.scala
@@ -10,35 +10,23 @@ import play.api.mvc._
 
 import scala.collection.JavaConverters._
 
-class HealthController extends Controller with DefaultInstrumented {
-  private[this] val healthCheckExecutor = Executors.newFixedThreadPool(3,
-    new ThreadFactoryBuilder().setNameFormat("health-check-%d").setDaemon(true).build())
+import java.util.Date
+import org.joda.time.DateTime
+
+
+
+
+class HealthController extends Controller  {
+  private[this] val startTime = System.currentTimeMillis()
 
   def health = Action {
-
-    val healthChecks = registry.runHealthChecks(healthCheckExecutor).asScala
-    val hasUnhealthyCheck = healthChecks.forall { case (_, result) =>
-      result.isHealthy
-    }
-
-    val body = healthChecks.map {
-      case (key, result) if result.isHealthy =>
-        healthBody(key, Option(result.getMessage).getOrElse("HEALTY"))
-      case (key, result) if !result.isHealthy && result.getError == null =>
-        healthBody(key, Option(result.getMessage).getOrElse("UNHEALTY"))
-      case (key, result) if !result.isHealthy && result.getError != null =>
-        healthBody(key, Option(result.getMessage).getOrElse("UNHEALTY"), Option(result.getError))
-    }.mkString("{", ", ", "}")
-
-    if (hasUnhealthyCheck) Ok(body).as(JSON) else ServiceUnavailable(body).as(JSON)
+    val uptimeInMillis = uptime()
+    Ok(s"{Status: Ok, Uptime: ${uptimeInMillis}ms, Date and Time: " + new DateTime(startTime)+ "}")
   }
 
-  private def healthBody(key: String, message: String, error: Option[Throwable] = None): String = {
-    error match {
-      case Some(exception) =>
-        s"\042$key\042:{\042message\042:\042$message\042,\042error\042:\042${Throwables.getStackTraceAsString(exception)}\042}"
-      case None =>
-        s"\042$key\042:\042$message\042"
-    }
+  private def uptime() : Long =  {
+    val uptimeInMillis = System.currentTimeMillis() - startTime
+    uptimeInMillis
   }
+
 }
