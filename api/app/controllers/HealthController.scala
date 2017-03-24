51c671a4e203fc93e314a85e8e5127377f5d265d
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-03-24 14:43:57 +0000

    Added ApplicationSpec tests for health/version
    - Changed HealthController to respond with JSON rather than plain text.
    - Removed unused imports from HealthController
    - Added VersionController/HealthController unit tests to ensure correct response format/text/status

--- api/app/controllers/HealthController.scala
+++ api/app/controllers/HealthController.scala
@@ -1,32 +1,19 @@
 package controllers
 
-import java.util.concurrent.Executors
-
-import com.google.common.base.Throwables
-import com.google.common.util.concurrent.ThreadFactoryBuilder
-import nl.grons.metrics.scala.DefaultInstrumented
 import play.api.mvc.Controller
 import play.api.mvc._
-
-import scala.collection.JavaConverters._
-
-import java.util.Date
 import org.joda.time.DateTime
-
-
-
 
 class HealthController extends Controller  {
   private[this] val startTime = System.currentTimeMillis()
 
   def health = Action {
     val uptimeInMillis = uptime()
-    Ok(s"{Status: Ok, Uptime: ${uptimeInMillis}ms, Date and Time: " + new DateTime(startTime)+ "}")
+    Ok(s"{Status: Ok, Uptime: ${uptimeInMillis}ms, Date and Time: " + new DateTime(startTime)+ "}").as(JSON)
   }
 
   private def uptime() : Long =  {
     val uptimeInMillis = System.currentTimeMillis() - startTime
     uptimeInMillis
   }
-
 }
