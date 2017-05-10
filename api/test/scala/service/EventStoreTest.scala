71ecb7ce7c38c676d7294bc732bceec9d9f86237
Volodymyr.Glushak <volodymyr.glushak@valtech.co.uk>
2017-05-10 09:16:47 +0100

    Implemented event storing in the HBase. Test fixing.

--- api/test/scala/service/EventStoreTest.scala
+++ api/test/scala/service/EventStoreTest.scala
@@ -8,25 +8,24 @@ import uk.gov.ons.bi.writers.BiConfigManager
 class EventStoreTest extends FlatSpec with Matchers with EventStore with BeforeAndAfterAll {
 
   private[this] val utility = HBaseTesting.hBaseServer
+  utility.createTable(tableName, columnFamily)
 
   "It" should "store events properly" in {
-    val before = utility.countRows(table)
     val instructions = "{command: DELETE, id: 1}"
     (0 to 10).foreach { x =>
       storeEvent(instructions)
     }
-    utility.countRows(table) shouldBe before + 11
-    getAll.length shouldBe before + 11
+    getAll.count { case (_, v) => v == instructions }  shouldBe 11
     val firstTime = getAll.headOption.map { case (k, _) => k.toLong }.getOrElse(sys.error("No data in HBase"))
-    getAll.tail.foreach { case (rc, _) =>
+    getAll.filter { case (_, v) => v == instructions } .tail.foreach { case (rc, _) =>
       rc.toLong shouldBe >(firstTime)
     }
     cleanAll()
     storeEvent(instructions)
-    getAll.length shouldBe before + 1
+    getAll.count { case (_, v) => v == instructions } shouldBe  1
   }
 
   override def config: Config = BiConfigManager.envConf(ConfigFactory.load())
 
-  override protected def tableName: String = config.getString("hbase.events.table.name")
+  override protected def tableName: String = config.getString("hbase.events.table.name") + "_es_test"
 }
