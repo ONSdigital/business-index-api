b1f1e8f466bbde544b00e69849033ad0a26f16b8
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-05-08 14:33:57 +0100

    Add ignored tests

--- test/scala/repository/ElasticSearchBusinessRepositoryTest.scala
+++ test/scala/repository/ElasticSearchBusinessRepositoryTest.scala
@@ -2,17 +2,17 @@ package scala.repository
 
 import java.time.Month._
 
-import com.sksamuel.elastic4s.http.{HttpExecutable, _}
+import com.sksamuel.elastic4s.http.{ HttpExecutable, _ }
 import config.ElasticSearchConfig
 import models.BusinessSearchRequest
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{FreeSpec, Matchers}
-import play.api.mvc.{AnyContent, Request}
+import org.scalatest.{ FreeSpec, Matchers }
+import play.api.mvc.{ AnyContent, Request }
 import play.core.j.HttpExecutionContext
 import repository.ElasticSearchBusinessRepository
 import utils.ElasticRequestMapper
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.sample.SampleBusiness
 
 /**
@@ -30,14 +30,14 @@ class ElasticSearchBusinessRepositoryTest extends FreeSpec with Matchers with Mo
   }
 
   private trait Fixture extends SampleBusiness {
-    val esRepository: HttpClient = mock[HttpClient]
+    val esClient: HttpClient = mock[HttpClient]
     val requestMapper: ElasticRequestMapper = mock[ElasticRequestMapper]
     val config = ElasticSearchConfig("bi-dev", "localhost", 9000, true)
-    val repository = new ElasticSearchBusinessRepository(esRepository, requestMapper, config)
+    val repository = new ElasticSearchBusinessRepository(esClient, requestMapper, config)
   }
 
   "An ElasticSearch Business Repository" - {
-    "can retrieve an enterprise unit by Enterprise Reference Number (ERN) and a period (yyyyMM) when a Enterprise exists" in new Fixture {
+    "can retrieve an enterprise unit by Enterprise Reference Number (ERN) and a period (yyyyMM) when a Enterprise exists" ignore new Fixture {
       //      (restRepository.findRow _).expects(TargetTable, TargetRowKey, DefaultColumnFamily).returning(Future.successful(Right(Some(ARow))))
       //      (rowMapper.fromRow _).expects(ARow).returning(Some(TargetEnterpriseUnit))
       //
@@ -46,35 +46,37 @@ class ElasticSearchBusinessRepositoryTest extends FreeSpec with Matchers with Mo
       //      }
     }
 
-    "can retrieve a business by id, when a business with that id exists" in new Fixture {
+    "can retrieve a business by id, when a business with that id exists" ignore new Fixture {
       //      (esRepository.execute _).expects()
-      1 shouldBe 1
     }
 
     "returns None when a business does not exist for a particular id" in new Fixture {
-      1 shouldBe 1
+      //      (esRepository.execute _).expects()
     }
 
-    "can retrieve search results for a search for BusinessName" in new Fixture with RequestFixture {
-      val searchQuery: BusinessSearchRequest = BusinessSearchRequest("BusinessName:test", request)
-      implicit val a = new HttpExecutable[BusinessSearchRequest, String] {
-        override def execute(client: HttpRequestClient, request: BusinessSearchRequest): Future[HttpResponse] = ???
-        override def responseHandler: ResponseHandler[String] = ???
-      }
+    "can retrieve search results for a search for BusinessName" ignore new Fixture with RequestFixture {
+      //      val searchQuery: BusinessSearchRequest = BusinessSearchRequest("BusinessName:test", request)
+      //
+      //
+      //      implicit val a = new HttpExecutable[BusinessSearchRequest, String] {
+      //        override def execute(client: HttpRequestClient, request: BusinessSearchRequest): Future[HttpResponse] = ???
+      //        override def responseHandler: ResponseHandler[String] = ???
+      //      }
+      //
+      //      val heaterStub = stub[HttpClient]
+      //
+      //      (heaterStub.execute _)(_).when().returns(true)
 
       // stub or mock a, syntax for stub different
+      // implicit val b = mock[HttpExecutable[BusinessSearchRequest, String]]
 
-      class HttpExecutable()
-
-      // (esRepository.execute _)(_: HttpExecutable[BusinessSearchRequest, String]).expects(searchQuery).returning(Some(123))
-      (esRepository.execute(_)(_)).expects(*).returning("123")
-      //(memcachedMock.get(_ : String)(_ : Codec)).expects("some_key", *).returning(Some(123))
-
-      1 shouldBe 1
+      //      (esRepository.execute _)(b).expects(*).returning(*)
+      // (esRepository.execute(_)(_)).expects(*).returning("123")
+      // (memcachedMock.get(_ : String)(_ : Codec)).expects("some_key", *).returning(Some(123))
     }
 
-    "returns None when a business search returns no results" in new Fixture {
-      1 shouldBe 1
+    "returns None when a business search returns no results" ignore new Fixture {
+      // (esRepository.execute _).expects()
     }
   }
 }
