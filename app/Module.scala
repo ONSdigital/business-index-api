dfabb475284286a9783297518549e513d8ec5468
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-05-24 14:54:32 +0100

    Add index creation code to the Module

--- app/Module.scala
+++ app/Module.scala
@@ -1,10 +1,13 @@
 import com.google.inject.AbstractModule
+import com.sksamuel.elastic4s.RefreshPolicy
+import com.sksamuel.elastic4s.analyzers._
 import com.sksamuel.elastic4s.http.ElasticDsl
-import play.api.{ Configuration, Environment }
+import play.api.{Configuration, Environment}
 import services.BusinessRepository
 import repository.ElasticSearchBusinessRepository
-import utils.{ ElasticClient, ElasticResponseMapper, ElasticResponseMapperSecured }
+import utils.{ElasticClient, ElasticResponseMapper, ElasticResponseMapperSecured}
 import config.ElasticSearchConfigLoader
+import models.IndexConsts._
 
 class Module(environment: Environment, configuration: Configuration) extends AbstractModule with ElasticDsl {
 
@@ -15,6 +18,40 @@ class Module(environment: Environment, configuration: Configuration) extends Abs
     val elasticSearchBusinessRepository = new ElasticSearchBusinessRepository(
       elasticSearchClient, new ElasticResponseMapper, new ElasticResponseMapperSecured, elasticConfig
     )
+
+    // Need to use config to choose whether or not to create index again etc.
+
+    // Firstly, we need to delete the index (we don't care if this fails)
+    elasticSearchClient.execute{
+      deleteIndex(elasticConfig.index)
+    }.await
+
+    // Next, we need to create the index with the correct mappings/analyzers etc.
+    elasticSearchClient.execute {
+      createIndex(elasticConfig.index).analysis(
+        CustomAnalyzerDefinition(cBiAnalyzer, WhitespaceTokenizer, LowercaseTokenFilter)
+      ).mappings(
+        mapping(cBiType).fields(
+          textField(cBiName).boost(cBiNameBoost).analyzer(cBiAnalyzer),
+          longField(cBiUprn).analyzer(KeywordAnalyzer),
+          textField(cBiPostCode).analyzer(cBiAnalyzer),
+          longField(cBiIndustryCode).analyzer(KeywordAnalyzer),
+          textField(cBiLegalStatus).analyzer(NotAnalyzed).includeInAll(false),
+          textField(cBiTradingStatus).analyzer(NotAnalyzed).includeInAll(false),
+          textField(cBiTurnover).analyzer(cBiAnalyzer),
+          textField(cBiEmploymentBand).analyzer(cBiAnalyzer),
+          textField(cBiPayeRefs).analyzer(KeywordAnalyzer),
+          longField(cBiVatRefs).analyzer(KeywordAnalyzer),
+          textField(cBiCompanyNo).analyzer(KeywordAnalyzer)
+        )
+      )
+    }.await
+
+    // Now we can insert some test data
+    elasticSearchClient.execute {
+      indexInto(elasticConfig.index / cBiType).doc(cBiName -> "Hello, world!").refresh(RefreshPolicy.IMMEDIATE)
+    }
+
     bind(classOf[BusinessRepository]).toInstance(elasticSearchBusinessRepository)
   }
 }
--- app/models/Business.scala
+++ app/models/Business.scala
@@ -96,3 +96,22 @@ object Business {
     companyNo = map.get(cBiCompanyNo).map(_.toString)
   )
 }
+
+object IndexConsts {
+  val cBiType = "business"
+  val cBiName = "BusinessName"
+  val cBiNameSuggest = "BusinessName_suggest"
+  val cBiUprn = "UPRN"
+  val cBiPostCode = "PostCode"
+  val cBiIndustryCode = "IndustryCode"
+  val cBiLegalStatus = "LegalStatus"
+  val cBiTradingStatus = "TradingStatus"
+  val cBiTurnover = "Turnover"
+  val cBiEmploymentBand = "EmploymentBands"
+  val cBiVatRefs = "VatRefs"
+  val cBiPayeRefs = "PayeRefs"
+  val cBiCompanyNo = "CompanyNo"
+  val cEmptyStr = ""
+  val cBiAnalyzer = "bi-analyzer"
+  val cBiNameBoost = 4
+}
