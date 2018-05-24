f40b9eb923e69833ad491e8ab82895329edaf66a
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-05-24 16:00:57 +0100

    Add code for inserting a row into the ES index

--- app/Module.scala
+++ app/Module.scala
@@ -1,17 +1,17 @@
 import com.google.inject.AbstractModule
-import com.sksamuel.elastic4s.RefreshPolicy
 import com.sksamuel.elastic4s.analyzers._
-import com.sksamuel.elastic4s.http.ElasticDsl
-import play.api.{Configuration, Environment}
+import play.api.{ Configuration, Environment }
 import services.BusinessRepository
 import repository.ElasticSearchBusinessRepository
-import utils.{ElasticClient, ElasticResponseMapper, ElasticResponseMapperSecured}
+import utils.{ ElasticClient, ElasticResponseMapper, ElasticResponseMapperSecured }
 import config.ElasticSearchConfigLoader
 import models.IndexConsts._
 
-class Module(environment: Environment, configuration: Configuration) extends AbstractModule with ElasticDsl {
+class Module(environment: Environment, configuration: Configuration) extends AbstractModule {
 
   override def configure(): Unit = {
+    import com.sksamuel.elastic4s.http.ElasticDsl._
+
     val underlyingConfig = configuration.underlying
     val elasticConfig = ElasticSearchConfigLoader.load(underlyingConfig)
     val elasticSearchClient = ElasticClient.getElasticClient(elasticConfig)
@@ -22,34 +22,40 @@ class Module(environment: Environment, configuration: Configuration) extends Abs
     // Need to use config to choose whether or not to create index again etc.
 
     // Firstly, we need to delete the index (we don't care if this fails)
-    elasticSearchClient.execute{
+    elasticSearchClient.execute {
       deleteIndex(elasticConfig.index)
     }.await
 
     // Next, we need to create the index with the correct mappings/analyzers etc.
-    elasticSearchClient.execute {
+    // Changes to index creation:
+    // - cannot use a KeywordAnalyzer on a long field
+    // - using KeywordAnalyzer instead of NotAnalyzed - achieves the same thing?
+    // - what does includeInAll do?
+    val a = elasticSearchClient.execute {
       createIndex(elasticConfig.index).analysis(
         CustomAnalyzerDefinition(cBiAnalyzer, WhitespaceTokenizer, LowercaseTokenFilter)
       ).mappings(
-        mapping(cBiType).fields(
-          textField(cBiName).boost(cBiNameBoost).analyzer(cBiAnalyzer),
-          longField(cBiUprn).analyzer(KeywordAnalyzer),
-          textField(cBiPostCode).analyzer(cBiAnalyzer),
-          longField(cBiIndustryCode).analyzer(KeywordAnalyzer),
-          textField(cBiLegalStatus).analyzer(NotAnalyzed).includeInAll(false),
-          textField(cBiTradingStatus).analyzer(NotAnalyzed).includeInAll(false),
-          textField(cBiTurnover).analyzer(cBiAnalyzer),
-          textField(cBiEmploymentBand).analyzer(cBiAnalyzer),
-          textField(cBiPayeRefs).analyzer(KeywordAnalyzer),
-          longField(cBiVatRefs).analyzer(KeywordAnalyzer),
-          textField(cBiCompanyNo).analyzer(KeywordAnalyzer)
+          mapping(cBiType).fields(
+            textField(cBiName).boost(cBiNameBoost).analyzer(cBiAnalyzer),
+            longField(cBiUprn),
+            textField(cBiPostCode).analyzer(cBiAnalyzer),
+            longField(cBiIndustryCode),
+            textField(cBiLegalStatus).analyzer(KeywordAnalyzer),
+            textField(cBiTradingStatus).analyzer(KeywordAnalyzer),
+            textField(cBiTurnover).analyzer(cBiAnalyzer),
+            textField(cBiEmploymentBand).analyzer(cBiAnalyzer),
+            textField(cBiPayeRefs).analyzer(KeywordAnalyzer),
+            longField(cBiVatRefs),
+            textField(cBiCompanyNo).analyzer(KeywordAnalyzer)
+          )
         )
-      )
     }.await
 
     // Now we can insert some test data
     elasticSearchClient.execute {
-      indexInto(elasticConfig.index / cBiType).doc(cBiName -> "Hello, world!").refresh(RefreshPolicy.IMMEDIATE)
+      indexInto(elasticConfig.index / cBiType).id("12345678").fields(
+        cBiName -> "Hello, world!"
+      )
     }
 
     bind(classOf[BusinessRepository]).toInstance(elasticSearchBusinessRepository)
